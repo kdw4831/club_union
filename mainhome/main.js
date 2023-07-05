5db9c02a339ad8808a66bbb467f8ac71d4072189
Deawon <19999eo@naver.com>
2023-07-05 16:34:07 +0900

    auto-slide

--- mainhome/main.js
+++ mainhome/main.js
@@ -1,6 +1,7 @@
 let curPos= 0; // 현재 보고 있는 이미지의 인덱스 번호!
 let position = 0; // 현재 .images 의 위치값!
 const IMAGE_WIDTH = 640; // 한번 움직일 때 이동해야 할 거리!
+let autoSlideInterval; // 자동 슬라이드를 위한 인터벌 변수
 
 // 요소 선택
 const prevBtn = document.querySelector(".prev")
@@ -38,12 +39,31 @@ function next(){
         nextBtn.setAttribute("disabled", 'true') // 못 누르는 버튼이 됨
     }
 }
+
+function startAutoSlide() {
+    autoSlideInterval = setInterval(() => {
+      next();
+    }, 3000); // 2초마다 다음 이미지로 슬라이드
+  }
+  
+  function stopAutoSlide() {
+    clearInterval(autoSlideInterval);
+  }
+
+
+  
+  
 // 초기 랜더링 시 최초 호출 함수의 관습적 이름
 function init(){
     // 앞으로 가기는 처음부터 못누르게!
     prevBtn.setAttribute("disabled", 'true')
     prevBtn.addEventListener("click", prev)
     nextBtn.addEventListener("click", next)
+
+
+    startAutoSlide(); // 자동 슬라이드 시작
+    images.addEventListener("mouseenter", stopAutoSlide); // 마우스가 이미지 위에 있을 때 자동 슬라이드 일시정지
+    images.addEventListener("mouseleave", startAutoSlide); // 마우스가 이미지에서 벗어날 때 자동 슬라이드 재시작
 }
 
 init();
