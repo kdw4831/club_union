afcdb0f9686eb9f13888b777bede448176254054
hyochan250 <hyochan250@gmail.com>
2023-07-13 14:45:30 +0900

    hyochan_branch

---
+++ club_application_system/process.php
@@ -0,0 +1,21 @@
+<?php
+    $con = mysqli_connect("localhost", "userid", "c1722172", "cookDB") or die ("MYSQL 접속 실패");
+    $sql = $_POST['sql'];
+    $ret = mysqli_query($con, $sql);
+
+    if ($_POST['type'] === "get_data") {
+        $ret_collectall = mysqli_query($con, $sql);
+        $rows = array();
+
+        while ($row = mysqli_fetch_assoc($ret_collectall)) { 
+            $rows[] = $row;
+        }
+        mysqli_close($con);
+        echo json_encode($rows);  
+        return;
+    };
+
+
+    mysqli_close($con);
+    echo "success";
+?>
