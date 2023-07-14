366543187ac93611d7772670cb6b4946d9856ff0
Deawon <19999eo@naver.com>
2023-07-14 21:44:40 +0900

    findID.js.comment

--- login_system/findID.js
+++ login_system/findID.js
@@ -1,5 +1,5 @@
-let mode="fID";
-let tabs=document.querySelectorAll(".findID-PWline div , a");
+let mode="fID";    // mode란 아이디찾기 비밀번호 찾기를 구분하기 위한 변수 선언, 초기에는  mode==fID 로 아이디 찾기가 우선적으로 보여지는 걸 말함
+let tabs=document.querySelectorAll(".findID-PWline div , a"); // tabs는 findID-PWline에 있는 div와 a를 선택해줌
 
 
 
@@ -7,7 +7,9 @@ let tabs=document.querySelectorAll(".findID-PWline div , a");
 
 
 
-
+// 여기서 우리가 필요한건  a 태그에 있는 fid와 fpw를 선택해야된다 그래서  for 문을 활용해 이 두개를 제외한 underline등을 없에준다
+//그래서 인덱스로 2,3이 선택되게 하고 클릭을 할때 선택한 a태그가 웹 콘솔창에 출력 된다, 그후 이 태그를 선택할 때 이벤트는 클릭으로 하고
+// 클릭이벤트가 일어났을 때 filter함수가 발생되도록 한다.
 for(let i=2; i<4; i++){
     console.log(tabs[i]);
     tabs[i].addEventListener("click",function(event){filter(event) })
@@ -18,7 +20,7 @@ for(let i=2; i<4; i++){
 
 function render(){
     
-    
+    // mode가 fid일 때  resultHTMl에 아이디 찾기 폼이 들어가게 한후 innerhtml로 웹화면에 출력되게 한다.
     if(mode=='fID'){
         let resultHtml=''
         resultHtml +=`
@@ -49,8 +51,8 @@ function render(){
                     </div>
                 </div>`
         document.getElementById("find-border2").innerHTML=resultHtml;
-
-    }else {
+    
+    }else { // mode가 fPW일 때  resultHTMl에 비밀번호 찾기 폼이 들어가게 한후 innerhtml로 웹화면에 출력되게 한다.
         let resultHtml=''
         resultHtml +=`
         <div class="find-border2">
@@ -98,10 +100,10 @@ function render(){
 
 
 function filter(event){
-    mode=event.target.id
+    mode=event.target.id // 여기서 event.target.id는  클릭했을 때 선택된 테그에 있는 id를 출력하게 하는것을 의미
     console.log(mode)
 
-    document.getElementById("under-line").style.width=
+    document.getElementById("under-line").style.width=            //이 부분은 youtube강의로 설명
     event.target.offsetWidth+"px";
 
     document.getElementById("under-line").style.top=
