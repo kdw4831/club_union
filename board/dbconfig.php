a6dd550cc2af336c41c451b645e3e52642944bad
Deawon <19999eo@naver.com>
2023-07-28 16:37:01 +0900

    delete revise button

--- board/dbconfig.php
+++ board/dbconfig.php
@@ -12,7 +12,7 @@ $db='board'; // 여기가 내가 sql로 작성한 부분이야 그니까
 try{
     $conn=new PDO("mysql: host=".$servername.";dbname=".$db, $username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "DB 연결 성공";
+    
 }catch(PDOException $e){
     echo "Connection failed:".$e->getMEssage();
 }
