e1744fbeaeece9ba2934917a1a84b277e865c9a2
박상언 <127275054+sangeon6248@users.noreply.github.com>
2023-08-10 14:40:47 +0900

    메인페이지 애니메이션 추가

--- Club_Home/test/script.js
+++ Club_Home/test/script.js
@@ -1,21 +1,20 @@
-function changePage(content){
-  location.href = `B.html?content=${content}`
-}
+const animatedText = document.querySelector('.animated-text');
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const content = urlParams.get('content');
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5 // When 50% of the element is visible
+};
 
-if (content) {
-  let titleText;
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show'); // Add 'show' class for animation
+            observer.unobserve(entry.target); // Stop observing once animation is triggered
+        }
+    });
+}, options);
 
-  if (content === 'abc') {
-    titleText = '안녕하세요';
-  } else if (content === 'cba') {
-    titleText = '환영합니다';
-  } else {
-    titleText = content;
-  }
-
-  document.getElementById("content").innerHTML = `내용: ${content}`;
-}
+// 이 부분에서 'animatedText' 대신 실제 사용하는 대상 요소의 선택자를 사용해야 합니다.
+// 예: const elementToObserve = document.querySelector('.some-element');
+observer.observe(animatedText);
